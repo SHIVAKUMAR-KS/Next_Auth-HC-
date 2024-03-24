ff336ec7b59388e25e8905f72dc40d88e4478ac9
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-03-24 17:41:27 +0530

    done with tested route for signup with postman

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,20 +1,24 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-export async function connect(){
-    try{
-        mongoose.connect(process.env.MONGODB_URI!)//string yaha hoga hi
-        const connection = mongoose.connection
-        connection.on('connected',()=>{
-            console.log('MongoDB Connected');
-            
+export async function connect() {
+    try {
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
         })
-        connection.on('error',(err)=>{
-            console.log('MongoDB Error,Please make sure db is up and running'+err);
+
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
             process.exit();
         })
-    }catch(error){
-        console.log('Something went wrong in connecting to DB');
+
+    } catch (error) {
+        console.log('Something goes wrong!');
         console.log(error);
         
     }
+
+
 }
